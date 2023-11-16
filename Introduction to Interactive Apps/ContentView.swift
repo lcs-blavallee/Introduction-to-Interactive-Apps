10da981d48f412da4cbc2a50b74454914c452c99
Benjamin Lavallee <blavallee@lcs.on.ca>
2023-11-16 09:19:37 -0500

    Added a stored property to hold the base of the power selected by the user.

--- Introduction to Interactive Apps/ContentView.swift
+++ Introduction to Interactive Apps/ContentView.swift
@@ -9,15 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     //MARK: Stored propterties
-    
+    @State var base: Int = 1
     
     //MARK: Computed properties
     var body: some View {
         VStack {
             Spacer()
-
             HStack(alignment:.top) {
-                Text("5")
+                Text("\(base)")
                     .font(.system(size: 96))
                 Text("2")
                     .font(.system(size: 44))
