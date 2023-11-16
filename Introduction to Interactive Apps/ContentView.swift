7384ac6bdbc70d9a7680a3cc751509be7f632f63
Benjamin Lavallee <blavallee@lcs.on.ca>
2023-11-16 08:53:12 -0500

    Initial Commit

---
+++ Introduction to Interactive Apps/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Introduction to Interactive Apps
+//
+//  Created by Benjamin Lavallee on 2023-11-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Introduction to Interactive Apps/Introduction_to_Interactive_AppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Introduction_to_Interactive_AppsApp.swift
+//  Introduction to Interactive Apps
+//
+//  Created by Benjamin Lavallee on 2023-11-16.
+//
+
+import SwiftUI
+
+@main
+struct Introduction_to_Interactive_AppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
