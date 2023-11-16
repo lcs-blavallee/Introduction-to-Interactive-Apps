e2c4d3250a75a75e5a673d4288554620a89a0ba0
Benjamin Lavallee <blavallee@lcs.on.ca>
2023-11-16 09:15:00 -0500

    Created the initial static layout

--- Introduction to Interactive Apps/ContentView.swift
+++ Introduction to Interactive Apps/ContentView.swift
@@ -8,12 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    //MARK: Stored propterties
+    
+    
+    //MARK: Computed properties
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+
+            HStack(alignment:.top) {
+                Text("5")
+                    .font(.system(size: 96))
+                Text("2")
+                    .font(.system(size: 44))
+                Text("=")
+                    .font(.system(size: 96))
+                Text("25")
+                    .font(.system(size: 96))
+            }
+            Spacer()
         }
         .padding()
     }
